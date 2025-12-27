eee59ca6e495849e711ec5c313f59e30bee18533
DevRodrigoPdp <rpuertadelpozo@gmail.com>
2025-12-27 13:23:16 +0100

    dom

---
+++ GestionDOM/Ejercicio5/script.js
@@ -0,0 +1,17 @@
+let resOrigen = document.getElementById("resOrigen")
+let resDestino = document.getElementById("resDestino")
+let resFechaIda = document.getElementById("resFechaIda")
+let resFechaVuelta = document.getElementById("resFechaVuelta")
+let resSoloIda = document.getElementById("resSoloIda")
+
+
+let destino = document.getElementById("destino")
+let origen = document.getElementById("origen")
+let fechaIda = document.getElementById("fechaIda")
+let fechaVuelta = document.getElementById("fechaVuelta")
+
+let soloIda = document.getElementById("soloIda")
+
+let btnSubmit = document.getElementById("btnSubmit")
+
+
